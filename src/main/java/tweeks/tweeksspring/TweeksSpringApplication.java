f285604d207156dcde15ee65361f95e4839c3b64
hyeg0121 <hyeg0121@naver.com>
2023-07-17 08:24:04 +0900

    init

---
+++ src/main/java/tweeks/tweeksspring/TweeksSpringApplication.java
@@ -0,0 +1,13 @@
+package tweeks.tweeksspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeksSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeksSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tweeks/tweeksspring/TweeksSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tweeks.tweeksspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeksSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
