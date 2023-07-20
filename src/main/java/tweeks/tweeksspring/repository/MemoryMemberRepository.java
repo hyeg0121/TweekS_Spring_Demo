a55d9b3b28e86e20688111c33ce2c59dd965cd19
hyeg0121 <hyeg0121@naver.com>
2023-07-20 13:48:28 +0900

    [:memo:] 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/tweeks/tweeksspring/repository/MemoryMemberRepository.java
+++ src/main/java/tweeks/tweeksspring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package tweeks.tweeksspring.repository;
 
+import org.springframework.stereotype.Repository;
 import tweeks.tweeksspring.domain.Member;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
