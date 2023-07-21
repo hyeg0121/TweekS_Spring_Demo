cefe32b0d69c46132c228886a0cc88b102ecdbac
hyeg0121 <hyeg0121@naver.com>
2023-07-21 15:02:57 +0900

    [:memo:] AOP 적용

---
+++ src/main/java/tweeks/tweeksspring/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package tweeks.tweeksspring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    // 패키지 하위에 모두 적용
+    @Around("execution(* tweeks.tweeksspring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs +
+                    "ms");
+        }
+    }
+}
